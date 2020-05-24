4bb8d2126190986d1464bc0c517c21413d9876c3
mutable-dan <{ID}+{username}@users.noreply.github.com>
2020-05-24 13:54:11 -0400

    added cpu # and finsihed rest of indicators

--- cpustat.cpp
+++ cpustat.cpp
@@ -129,6 +129,7 @@ void getcpuInfo( vcpu& a_vci )
  */
 void calculateCpu( const vcpu& a_vci_1, const vcpu& a_vci_2 )
 {
+    int16_t nIndex = 0;
     auto itcpu2 = a_vci_2.begin();
     for( cpuInfo cpu1 : a_vci_1 )
     {
@@ -138,13 +139,40 @@ void calculateCpu( const vcpu& a_vci_1, const vcpu& a_vci_2 )
         double sum1 = cpu1.usr + cpu1.nice + cpu1.sys + cpu1.idle + cpu1.iowait + cpu1.irq + cpu1.sirq + cpu1.steal + cpu1.guest + cpu1.guestnice;
         double sum2 = cpu2.usr + cpu2.nice + cpu2.sys + cpu2.idle + cpu2.iowait + cpu2.irq + cpu2.sirq + cpu2.steal + cpu2.guest + cpu2.guestnice;
         
-        double diff_usr = (double)(cpu2.usr - cpu1.usr);
-        double pct_usr = diff_usr/(sum2-sum1)*100.0;
+        double diff_usr       = (double)(cpu2.usr       - cpu1.usr);
+        double diff_nice      = (double)(cpu2.nice      - cpu1.nice);
+        double diff_sys       = (double)(cpu2.sys       - cpu1.sys);
+        double diff_idle      = (double)(cpu2.idle      - cpu1.idle);
+        double diff_iow       = (double)(cpu2.iowait    - cpu1.iowait);
+        double diff_irq       = (double)(cpu2.irq       - cpu1.irq);
+        double diff_sirq      = (double)(cpu2.sirq      - cpu1.sirq);
+        double diff_steal     = (double)(cpu2.steal     - cpu1.steal);
+        double diff_guest     = (double)(cpu2.guest     - cpu1.guest);
+        double diff_guestnice = (double)(cpu2.guestnice - cpu1.guestnice);
 
-        double diff_sys = (double)(cpu2.sys - cpu1.sys);
-        double pct_sys = diff_sys/(sum2-sum1)*100.0;
 
-        printf( "%%usr:%5.1lf %%sys:%5.1lf\n", pct_usr, pct_sys );
+        double pct_usr       = diff_usr      / (sum2-sum1)*100.0;
+        double pct_nice      = diff_nice     / (sum2-sum1)*100.0;
+        double pct_sys       = diff_sys      / (sum2-sum1)*100.0;
+        double pct_idle      = diff_idle     / (sum2-sum1)*100.0;
+        double pct_iow       = diff_iow      / (sum2-sum1)*100.0;
+        double pct_irq       = diff_irq      / (sum2-sum1)*100.0;
+        double pct_sirq      = diff_sirq     / (sum2-sum1)*100.0;
+        double pct_steal     = diff_steal    / (sum2-sum1)*100.0;
+        double pct_guest     = diff_guest    / (sum2-sum1)*100.0;
+        double pct_guestnice = diff_guestnice/ (sum2-sum1)*100.0;
+
+        if( nIndex == 0 )
+        {
+            printf( "cpu ave: %%usr:%5.1lf %%nice:%5.1lf %%sys:%5.1lf %%idle:%5.1lf %%iowait:%5.1lf %%irq:%5.1lf %%sirq:%5.1lf %%steal:%5.1lf %%guest:%5.1lf %%guest nice:%5.1lf\n", 
+                pct_usr, pct_nice, pct_sys, pct_idle, pct_iow, pct_irq, pct_sirq, pct_steal, pct_guest, pct_guestnice );
+
+        } else
+        {
+            printf( "cpu%02d  : %%usr:%5.1lf %%nice:%5.1lf %%sys:%5.1lf %%idle:%5.1lf %%iowait:%5.1lf %%irq:%5.1lf %%sirq:%5.1lf %%steal:%5.1lf %%guest:%5.1lf %%guest nice:%5.1lf\n", 
+                nIndex-1, pct_usr, pct_nice, pct_sys, pct_idle, pct_iow, pct_irq, pct_sirq, pct_steal, pct_guest, pct_guestnice );
+        }
+        ++nIndex;
     }
 }
 
